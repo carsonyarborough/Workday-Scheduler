5edcc9b0d2ad9cff0d38c3e3d150636f1070f17a
carsonyarborough <carson.yarborough@gmail.com>
2022-07-25 21:24:03 -0500

    created time displays and sections for appointments

---
+++ script.js
@@ -0,0 +1,12 @@
+var rightNow = moment().format('MMMM Do YYYY, h:mm:ss a');
+$()
+var timeEl = $("#currentDay");
+
+timeEl.text(rightNow);
+
+// function displayTime(){
+//     var interval = setInterval(rightNow,1000)
+//     timeEl.text(interval)
+// }
+// displayTime()
+//setInterval(rightNow, 1000);
