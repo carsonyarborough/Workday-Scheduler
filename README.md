fd1e6e029c127a7bebda0b42792b41b965d22d87
carsonyarborough <carson.yarborough@gmail.com>
2022-08-12 09:20:20 -0500

    added javascript to colorcoordinate hours

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Workday-Scheduler
-Scheduling a workday with third party APIs.
+This challenge I worked with existing code to create a workday scheduler that helps you plan out the rest of your work day. 
+
+
+## What I learned
+I learned how to use if/else if statements to color coordinate the hours of the day based on the current time of day. I used html to add icons to the save buttons and code all the timeblocks. Moment.js is used to display the current time of day the webpage was opened at and refreshes when the page is. 
+
+## Links
+[Repo Link](https://github.com/carsonyarborough/Workday-Scheduler)
+
+[Deployed Application](https://carsonyarborough.github.io/Workday-Scheduler/)
--- script.js
+++ script.js
@@ -1,12 +1,60 @@
 var rightNow = moment().format('MMMM Do YYYY, h:mm:ss a');
 $()
+// var saveBtnEl = $(".saveBtn")
+// saveBtnEl.on('click', function() {
+//     console.log(this)
+//    var hourlyInput = $(this).parent().attr("id")
+//    var inputText = $(this).parent('description').val()
+// });
 var timeEl = $("#currentDay");
 
 timeEl.text(rightNow);
 
+// var timeblock = $("#time-block");
+
+var currentHour = moment().hour();
+console.log(currentHour);
+
+function timeSlotColor(){
+    $(".time-block").each(function() {
+        var timeBlockId = parseInt($(this).attr("id"))
+        console.log("timeblock in the loop is"+timeBlockId,currentHour);
+        if (currentHour === timeBlockId){
+            $(this).addClass("present")
+            $(this).removeClass("past")
+            $(this).removeClass("future")
+        }
+        else if(currentHour > timeBlockId){
+            $(this).addClass("past")
+            $(this).removeClass("present")
+            $(this).removeClass("future")
+        }
+        else {
+            $(this).addClass("future")
+            $(this).addClass("past")
+            $(this).addClass("present")
+        }
+      });
+} 
+timeSlotColor();
+
+
+
+
+// need to use window storage to save text thats typed into the text area 
+
+
+// if (currentHour === currentHour) {
+//  var presentHour = $(this).parent("hr-nine").attr()
+// }
+// var textArea = 
+// 
 // function displayTime(){
 //     var interval = setInterval(rightNow,1000)
 //     timeEl.text(interval)
 // }
 // displayTime()
 //setInterval(rightNow, 1000);
+
+// if (rightNow === )
+// function currentTime(){
